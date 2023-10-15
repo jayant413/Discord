6297216dee9df3287794d27626f2a86ffcf419ef
jayant413 <jayantsawarkar4103@gmail.com>
2023-10-15 11:19:38 +0530

    Create: Clerk Authentication , Shadcn UI

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,13 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
+
+const HomePage = () => {
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
+    </div>
+  );
+};
+
+export default HomePage;
